a1e33d4f1ec110f5407bd591f8046ca8bee05722
jbaKYbN5p3jXnSFk <jbaKYbN5p3jXnSFK@gmail.com>
2021-10-03 19:51:05 -0400

    updated repl and main

--- src/main/java/edu/brown/cs/student/main/Database.java
+++ src/main/java/edu/brown/cs/student/main/Database.java
@@ -1,2 +1,104 @@
-package edu.brown.cs.student.main;public class Database {
-}
+package edu.brown.cs.student.main;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+    /**
+     * Autocorrect but with databases.
+     * <p>
+     * Chooses to pass SQL exceptions on to the class that instantiates it.
+     */
+    public class Database {
+
+
+        private static Connection conn = null;
+        private static ArrayList<INode> nodes = new ArrayList<>();
+        private static HashMap<INode, Integer> userMap = new HashMap<>();
+        private static HashMap<Integer, INode> idMap = new HashMap<>();
+
+        /**
+         * Instantiates the database
+         * Automatically loads files.
+         *
+         * @param filename file name of SQLite3 database to open.
+         * @throws SQLException if an error occurs in any SQL query.
+         */
+        Database(String filename) throws SQLException, ClassNotFoundException {
+
+            Class.forName("org.sqlite.JDBC");
+            String urlToDB = "jdbc:sqlite:" + filename; //not sure if filename is in correct format
+            conn = DriverManager.getConnection(urlToDB);
+            Statement stat = conn.createStatement();
+            stat.executeUpdate("PRAGMA foreign_keys=ON;");
+            stat.close();
+
+            PreparedStatement prep;
+            prep = conn.prepareStatement("");
+        }
+
+
+        /**
+         * Reads all the rows from the database into a user list.
+         *
+         *
+         * @throws SQLException if something goes wrong with the SQL
+         */
+        void populateUserListAndMap() throws SQLException {
+            PreparedStatement prep = conn.prepareStatement("SELECT users.weight, users.height, users.age, users.user_id FROM users;");
+            ResultSet rs = prep.executeQuery();
+            while (rs.next()) {
+                String weight = rs.getString(1);
+                String height = rs.getString(2);
+                String age = rs.getString(3);
+                String id = rs.getString(4);
+                weight = weight.substring(0, weight.length() - 2);
+                double weightNum = Double.parseDouble(weight);
+                height = height.charAt(0) + height.substring(3, height.length() - 1); //substring in format (510)
+                double heightNum = (Double.parseDouble(String.valueOf(height.charAt(0))) * 12) + Double.parseDouble(height.substring(1, height.length())); //not sure why i need valueof
+                double ageNum = Double.parseDouble(age);
+                Integer idInt = Integer.parseInt(id);
+                ThreeDimNode user = new ThreeDimNode(weightNum, heightNum, ageNum);
+                nodes.add(user);
+                userMap.put(user, idInt);
+                idMap.put(idInt, user);
+
+            }
+            rs.close();
+            prep.close();
+        }
+
+        void populateHoroscopeCount() throws SQLException{
+            //sql query to
+        }
+
+        /**
+         * getter to return user list
+         * @return list of users
+         */
+        ArrayList<INode> getUserList(){
+            return nodes;
+        }
+        /**
+         * getter to return hashmap of users to id
+         * @return userMap with user ID's mapped to users
+         */
+        HashMap<INode, Integer> getUserMap() {
+            return userMap;
+        }
+
+        /**
+         * getter to return hashmap of id to users
+         * @return userMap with user ID's mapped to users
+         */
+        HashMap<Integer, INode> getIdMap() {
+            return idMap;
+        }
+
+    }
+
+
+
+
--- src/main/java/edu/brown/cs/student/main/REPL.java
+++ src/main/java/edu/brown/cs/student/main/REPL.java
@@ -3,22 +3,30 @@ package edu.brown.cs.student.main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.*;
 
 /**
  * The REPL class for this lab.
  */
 public class REPL {
-
+//global variables to use after data from sqlite has been loaded in
   HashMap<String, Object> commands;
+  KDTree builtTree;
+  ArrayList<INode> builtUserList;
+  HashMap<INode, Integer> builtUserMap;
+  HashMap<Integer, INode> builtIdMap;
+
+
   /**
    * instantiates a REPL.
    *
    * @param commands - a map of string command names to IReplMethod objects, which
    *                 are a wrapper for a method to be called
    */
-  // TODO: create a more generic interface for Handler
+  // create a more generic interface for Handler
   public REPL(HashMap<String, Object> commands) {
     this.commands = commands;
   }
@@ -56,44 +64,77 @@ public class REPL {
               if (next_cmd.equals("online")) {
                 // TODO (For API ppl) do something that's related to the API pursuers, please fill this part in
               }
-//              try {
-//                // TODO (for ORM) call a method that handles reading in sqlite files
-//              } catch (IOException e) {
-//                errorHandler.filePathException();
-//              }
+              try {
+//                //  (for ORM) call a method that handles reading in sqlite files
+                //TODO call construct tree
+                Database allUserData = new Database(next_cmd);
+                allUserData.populateUserListAndMap();
+                builtUserList = allUserData.getUserList();
+                builtTree =  new KDTree(builtUserList.size(), builtUserList);
+                builtUserMap = allUserData.getUserMap();
+                builtIdMap = allUserData.getIdMap();
+                //construct tree
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
               break;
             case "similar":
-              // TODO create a Handler that "prints out the user_ids of the most similar k users", either searching by user_id or
-              // weights, heights, and ages
-              break;
-            case "classify": {
-//              String name;
-//              if (st.hasMoreTokens()) {
-//                name = st.nextToken();
-//              } else {
-//                name = "";
-//              }
-              // TODO create a Handler to print out a horoscope comparison chart of the k most similar users [closest in euclidean distance of weights, heights, and ages]
-              //either by "similar k some_user_id" or "similar k weight height age"
+              LinkedList<String> arguments = new LinkedList<String>();
+              while (st.hasMoreTokens()) { //if there are more commands after "similar"
+                String cmd = st.nextToken(); //cmd1 is k
+                arguments.add(cmd);
+              }
+                if (arguments.size() == 2 ) { //cmd0 is command, cmd1 is k, cmd2 is id
+                  String id = arguments.get(2); //get argument id
+                  Integer idInt = Integer.valueOf(id); //argument id to integer
+                  List<INode> nearest = builtTree.findKNearest(builtIdMap.get(idInt)); //find nearest list of INodes
+                  for (INode user : nearest) { //for every element in the list of INodes
+                    System.out.println(builtUserMap.get(user)); //user the user to id map to print the id's
+                  }
+                if (arguments.size() == 4) { //0 is command, 1 is k, 2 is weight, 3 height, 4 age
+                  String weight = arguments.get(2);
+                  String height = arguments.get(3);
+                  String age = arguments.get(4);
+                  weight = weight.substring(0, weight.length() - 2);
+                  double weightNum = Double.parseDouble(weight);
+                  height = height.charAt(0) + height.substring(3, height.length() - 1); //substring in format (510)
+                  double heightNum = (Double.parseDouble(String.valueOf(height.charAt(0))) * 12) + Double.parseDouble(height.substring(1, height.length())); //not sure why i need valueof
+                  double ageNum = Double.parseDouble(age);
+                  ThreeDimNode userToCompareTo = new ThreeDimNode(weightNum, heightNum, ageNum);
+                  List<INode> nearestUser = builtTree.findKNearest(userToCompareTo);
+                  for (INode user : nearestUser) { //for every element in the list of INodes
+                    System.out.println(builtUserMap.get(user));
+                  }
+                }
+                  break;
+       //         case "classify": {
+        //          String name;
+        //          if (st.hasMoreTokens()) {
+         //           name = st.nextToken();
+         //         } else {
+         //           name = "";
+          //        }
+                  // TODO create a Handler to print out a horoscope comparison chart of the k most similar users [closest in euclidean distance of weights, heights, and ages]
+                  //either by "similar k some_user_id" or "similar k weight height age"
 
-              break;
-            }
-            default:  // command unrecognized
-              errorHandler.inputFormatException();
-              break;
+                 // break;
+                }
+                default:  // command unrecognized
+                  errorHandler.inputFormatException();
+                  break;
+              }
           }
+        } catch(IOException e){ // some kind of read error, so the repl exits
+          errorHandler.parseInputException();
+          break;
         }
-      } catch (IOException e) { // some kind of read error, so the repl exits
-        errorHandler.parseInputException();
-        break;
-      }
-    }
+
     try {
       reader.close();
     } catch (IOException e) {
       errorHandler.closeReaderException();
     }
   }
-}
+}}
 
 
